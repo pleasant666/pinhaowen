8323b99b1ccebd00291cc866d31d6e4a5b46b284
pleasant666 <pleasant666@qq.com>
2025-08-09 18:33:38 +0800

    Add Header and Footer components
    Introduced Header.vue and Footer.vue components with basic template markup for site branding and copyright information.
    
    Co-Authored-By: モル <82874689+MorrisAFK@users.noreply.github.com>

---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="footer">
+    <p>all right reserved: persand.com 2025 copyright</p>
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="header">
+    <h1>小破岛·拼好文</h1>
+    <p>
+      <i>Lorem ipsum dolor sit <b>amet consectetur</b></i> adipiscing elit quisque
+      <b>faucibus <i>ex sapien vitae</i> pellentesque</b> sem placerat in id cursus mi.
+    </p>
+  </div>
+</template>
